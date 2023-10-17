409530efbfc0718a48943304693ff504c71863a7
PiusLucky <luckypius50@gmail.com>
2023-10-17 08:41:25 +0100

    chore: updated the ping field in health_check

--- app/routes/health_check_route.py
+++ app/routes/health_check_route.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from datetime import datetime
 
 health_check_route = Blueprint('health_check_route', __name__)
 
@@ -8,5 +9,5 @@ def health_check():
     return {
         "status": 200,
         "message": "Welcome to Task Master API v1",
-        "ping": "2023-09-30T12:12:13.690Z"
+        "ping": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
